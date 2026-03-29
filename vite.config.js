75b4dcca88a066b609a6e69075252855048b1752
Holger Grosser <holger.grosser@iso9001.info>
2026-03-29 05:36:36 +0200

    fix: polyfill node builtins for docx

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
+import polyfillNode from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis'
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer/',
+      process: 'process/browser'
+    }
+  },
+  optimizeDeps: {
+    include: ['buffer', 'process']
+  },
   build: {
     outDir: 'dist',
-    minify: 'esbuild'
+    minify: 'esbuild',
+    rollupOptions: {
+      plugins: [
+        polyfillNode(),
+        inject({
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process'
+        })
+      ]
+    }
   }
 })
